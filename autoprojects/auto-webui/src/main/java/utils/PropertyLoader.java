8ef1db517f2ca8807921d69c37f7530d58aaba6c
dungau <meocontinhnghich341@yahoo.com.vn>
2021-09-26 14:50:43 +0700

    Create properties file for test-data. Read properties file with mvn cmd

---
+++ autoprojects/auto-webui/src/main/java/utils/PropertyLoader.java
@@ -0,0 +1,67 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyLoader {
+
+    public static WebDriver getDriver() {
+
+        String browser = readProperty("browserType");
+        WebDriver driver = null;
+
+        switch (browser) {
+            case "firefox" -> {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            }
+            case "chrome" -> {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            }
+            case "ie" -> {
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+            }
+        }
+        return driver;
+    }
+
+    private static String readProperty(String name) {
+        try {
+            Properties props = new Properties();
+            props.load(PropertyLoader.class.getResourceAsStream(getPropertiesPath()));
+            return props.getProperty(name);
+        }
+        catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public static String getDomain() {
+        return readProperty("domainUrl");
+    }
+
+    public static Integer getTimeOutInSeconds() {
+        int timeOutInSeconds = 0;
+        try{
+            timeOutInSeconds = Integer.parseInt(
+                    readProperty("timeOutInSeconds"));
+        }
+        catch (NumberFormatException ex){
+            ex.printStackTrace();
+        }
+        return timeOutInSeconds;
+    }
+
+    private static String getPropertiesPath() {
+        String env = System.getProperty("environment");
+        return String.format("/%s-config.properties", env != null ? env : "test");
+    }
+}
--- autoprojects/auto-webui/src/test/java/pages/LoginTest.java
+++ autoprojects/auto-webui/src/test/java/pages/LoginTest.java
@@ -3,32 +3,44 @@ package pages;
 import entities.Users;
 import static org.testng.Assert.*;
 
-import org.testng.annotations.BeforeTest;
+import io.qameta.allure.Description;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static pages.SwagLabsPage.loginPage;
-
 
 public class LoginTest extends BaseTest {
 
     private Users user;
 
-    @BeforeTest
-    public void beforeTest() {
+    @BeforeMethod
+    public void init() {
         loginPage = new LoginPage(driver);
         user = new Users();
     }
 
+    @Description("Verify that user is able to login with valid credentials")
     @Test
     public void testLoginWithValidCredentials() {
         loginPage.loginWithValidCredentials(user.getDefaultUser());
     }
 
+    @Description("Verify that an error message shows when logged with blank credentials")
     @Test
-    public void testLoginWithInvalidCredentials() {
+    public void testLoginWithBlankCredentials() {
         String errorMessage = loginPage.loginWithInvalidCredential(user);
         assertEquals(errorMessage, "Please enter a username and password.",
-                "Can not find the error message.");
+                "The error message does not match.");
+    }
+
+    @Description("Verify that an error message shows when logged with invalid credentials")
+    @Test
+    public void testLoginWithInvalidCredential() {
+        user.setUsername("test");
+        user.setPassword("test");
+        String errorMessage = loginPage.loginWithInvalidCredential(user);
+        assertEquals(errorMessage, "The username and password could not be verified.",
+                "The error message does not match.");
     }
 
 }
