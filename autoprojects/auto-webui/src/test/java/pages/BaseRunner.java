50e987fa8b397c59b6fb034a0a1894c826761a65
dungau <meocontinhnghich341@yahoo.com.vn>
2021-10-18 09:51:10 +0700

    convert selenium web ui to BDD styles

--- autoprojects/auto-webui/src/test/java/pages/BaseRunner.java
+++ autoprojects/auto-webui/src/test/java/pages/BaseRunner.java
@@ -3,7 +3,7 @@ package pages;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions( features = {"src/test/pages/"}
+@CucumberOptions( features = {"src/test/java/pages"}
         , glue = { "steps" })
 public class BaseRunner extends AbstractTestNGCucumberTests {
 
